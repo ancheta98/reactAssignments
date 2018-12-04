20274e9d92cc8fd4294757f08e6d965d9c5929dc
Angel Ancheta <angelancheta98@yahoo.com>
2018-12-04 12:30:01 -0800

    finished assignment

--- event/src/App.js
+++ event/src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  inputHandler = (e) => {console.log(e.target.value)}
+  state = {
+    counter: this.props.initVal
+  };
+  inputHandler = e => {
+    console.log(e.target.value);
+  };
+  clickHandler = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
   render() {
     return (
       <div className="App">
         <h2>Event</h2>
-        <input onChange={this.inputHandler}/>
+        <input onChange={this.inputHandler} />
+        <button onClick={this.clickHandler}>click to increment</button>
+        <p>{this.state.counter}</p>
       </div>
     );
   }
