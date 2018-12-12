9c89fb2ac9b230659d2a8d585e7750eb3e846db3
Angel Ancheta <angelancheta98@yahoo.com>
2018-12-11 20:55:04 -0800

    adding better readmes

--- event/src/App.js
+++ event/src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    text: "",
     counter: 0
   }
   inputHandler = e => {
-    console.log(e.target.value);
+    console.log(e.target.value)
+    this.setState({
+      text: e.target.value
+    })
   };
   clickHandler = () => {
     this.setState({
@@ -18,6 +22,8 @@ class App extends Component {
       <div className="App">
         <h2>Event</h2>
         <input onChange={this.inputHandler} />
+        <h4>See your input mirrored below!</h4>
+        <p>{this.state.text}</p>
         <br/>
         <h2>Button incrementor</h2>
         <button onClick={this.clickHandler}>click to increment</button>
