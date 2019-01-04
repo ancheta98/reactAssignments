a1daa63ec9572f80a5ee0098498fe152d5aa9227
Angel Ancheta <angelancheta98@yahoo.com>
2019-01-04 09:43:51 -0800

    nice nice

--- advrenderingremove/README.md
+++ advrenderingremove/README.md
@@ -1,4 +1,5 @@
 # Based on the other advanced rendering assignment
 Users can now also click to delete a language from the list. Data does not persist, the list will reset upon refreshing.
+Update: fixed assignment so that key warning goes away
 ![(event example)](../assets/advRenderingRemove.png)
 ![(event example)](../assets/advRenderingRemove2.png)
--- advrenderingremove/src/App.js
+++ advrenderingremove/src/App.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {languages: ["Javascript", "Java", "Python","PHP","C++","C+","TypeScript","Shell","C","Ruby"]}
+  state = { languages: [{ key: 1, val: "Javascript" }, { key: 2, val: "Java" }, { key: 3, val: "Python" }, { key: 4, val: "PHP" }, { key: 5, val: "C++" }, { key: 6, val: "C+" }, { key: 7, val: "TypeScript" }, { key: 8, val: "Shell" }, { key: 9, val: "C" }, { key: 10, val: "Ruby" }] }
   render() {
-    const popLangs = this.state.languages.map( (lang, index) => 
-      <div onClick={() => {
+    const popLangs = this.state.languages.map((lang, index) =>
+      <div key={lang.key} onClick={() => {
         let temp = [...this.state.languages];
-        temp.splice(index,1);
-        this.setState({languages: temp})
+        temp.splice(index, 1);
+        this.setState({ languages: temp })
       }}>
-        <h4>Place: {index+1}</h4>
-        <p>{lang}</p>
+        <h4>Place: {index + 1}</h4>
+        <p>{lang.val}</p>
         <hr />
       </div>
-      )
+    )
 
     return (
       <div className="App">
-      <h2>Most popular github languages | Click to remove</h2>
+        <h2>Most popular github languages | Click to remove</h2>
         {popLangs}
       </div>
     );
